e9a0a8737c975b2d4fc498caf2988a8c64c11633
hsz <1265243465@qq.com>
2024-05-27 20:34:53 +0800

    ris_SpringBoot first

---
+++ ris/code/ris_SpringBoot/src/test/java/com/by/ris_springboot/RisSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.by.ris_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RisSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
